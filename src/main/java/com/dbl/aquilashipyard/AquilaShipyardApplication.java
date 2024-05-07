e7d8fdd213c1591269f9ea8d5205394f1dcecd94
vitor <vitorhugosilva2009@hotmail.com>
2024-05-06 21:35:06 -0300

    Start Project

---
+++ src/main/java/com/dbl/aquilashipyard/AquilaShipyardApplication.java
@@ -0,0 +1,13 @@
+package com.dbl.aquilashipyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquilaShipyardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquilaShipyardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbl/aquilashipyard/AquilaShipyardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbl.aquilashipyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquilaShipyardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
